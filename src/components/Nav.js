d4da7e22661461a380c0e58f1689b257644dcd5c
Vitor Lourenco <vitor.registros@lourencos.net>
2021-05-20 12:39:27 -0300

    Change layout of the logout button

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -10,14 +10,18 @@ export default function Nav(props){
 }
 
 const Navbar = styled.nav`
-  display: ${({isShowing})=>isShowing?"initial":"none"};
+  display: ${({isShowing})=>isShowing?"flex":"none"};
+  height: 30px;
+  box-shadow: 0 0 4px 4px rgba(0,0,0,0.3);
   position: absolute;
   background-color: white;
   border-radius: 10px;
   padding: 0 5px 3px 5px;
   cursor: pointer;
   right: 70px;
-  top: 20px;
+  top: 14px;
+  font-size: 14px;
+  align-items: center;
   &:hover{
     color: red;
   }
