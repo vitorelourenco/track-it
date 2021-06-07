b0dcfcda6116194bfbaf760b82a4f721e9ca7969
Bruna Topan <brutopanb@gmail.com>
2021-06-07 16:40:33 -0300

    Preparando review

--- src/contexts/TodaysContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react';
-
-const TodaysContext = createContext();
-
-export default TodaysContext;
--- src/functions/logout.js
+++
@@ -1,4 +0,0 @@
-export default function logout() {
-  localStorage.clear();
-  document.location.href = "/";
-}
